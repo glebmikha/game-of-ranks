3325f94f12979407e5b91676370d90966b67aaea
Gleb Mikhaylov <gm.mikhailov@gmail.com>
2019-06-01 15:35:43 +0300

    added question component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="container">
     <app-header :numberCount="7" :maxNumbers="12"></app-header>
-    <average-error>14</average-error>
+    <question :qRank="3"></question>
     <number-grid :numbers="numbers"></number-grid>
+    <average-error>14</average-error>
     <app-footer></app-footer>
   </div>
 </template>
@@ -12,12 +13,14 @@ import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 import AverageError from "./components/AverageError.vue";
 import NumberGrid from "./components/NumberGrid.vue";
+import Question from "./components/Question.vue";
 export default {
   components: {
     appHeader: Header,
     appFooter: Footer,
     averageError: AverageError,
-    numberGrid: NumberGrid
+    numberGrid: NumberGrid,
+    question: Question
   },
   data: function() {
     return {
---
+++ src/components/Question.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+    <div class="col-sm-8 offset-sm-2 col-xs-12 col-md-6 offset-md-3 text-center">
+      <h1>
+        Find element with rank
+        <span>{{ qRank }}</span>
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["qRank"]
+};
+</script>
+
+<style scoped>
+span {
+  color: green;
+}
+</style>
