daae7eff9517a1a7487ff518a2629e5888dfc3cd
Gleb Mikhaylov <gm.mikhailov@gmail.com>
2019-06-01 14:14:45 +0300

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+    <div class="col-sm-12">
+      <h3>Game Of Ranks</h3>
+      <div class="progress">
+        <div class="progress">
+          <div
+            class="progress-bar"
+            role="progressbar"
+            aria-valuenow="60"
+            aria-valuemin="0"
+            aria-valuemax="100"
+            :style="{width: (numberCount / maxNumbers) * 100 + '%'}"
+          >{{ numberCount }} / {{ maxNumbers}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["numberCount", "maxNumbers"]
+};
+</script>
