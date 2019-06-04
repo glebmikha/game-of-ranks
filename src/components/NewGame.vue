667ecb7d7554bacb07931b82848d857b11615937
Gleb Mikhaylov <gm.mikhailov@gmail.com>
2019-06-04 11:44:20 +0300

    added difficuly levels

--- src/components/NewGame.vue
+++ src/components/NewGame.vue
@@ -3,27 +3,43 @@
     <div class="col-sm-8 offset-sm-2 col-xs-12 col-md-6 offset-md-3 text-center">
       <button class="btn btn-success" @click="startNewGame">Start New Game</button>
     </div>
+
+    <div class="col-sm-8 offset-sm-2 col-xs-12 col-md-6 offset-md-3 text-center mt-3">
+      <label for="male">
+        <input type="radio" id="easy" value="12" v-model="difficulty"> Easy
+      </label>
+      <label for="female">
+        <input type="radio" id="medium" value="24" v-model="difficulty"> Medium
+      </label>
+      <label for="female">
+        <input type="radio" id="hard" value="36" v-model="difficulty"> Hard
+      </label>
+    </div>
   </div>
 </template>
 
 <script>
-const N_NUMBERS = 12;
 const COEF = 120;
 
 export default {
   data: function() {
     return {
-      nNumbers: N_NUMBERS,
-      coef: COEF
+      coef: COEF,
+      difficulty: 12
     };
   },
   methods: {
     startNewGame() {
       this.$emit("startNewGame", {
-        nNumbers: this.nNumbers,
+        nNumbers: this.difficulty,
         coef: this.coef
       });
     }
   }
 };
 </script>
+<style>
+label {
+  margin-right: 1rem;
+}
+</style>
